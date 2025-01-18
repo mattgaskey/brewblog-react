2172b4e5064a1ebb0a79d4066a0d9a74e951aa8e
Matt Gaskey <mgaskey@insidenewcity.com>
2025-01-17 18:02:53 -0800

    styling

--- src/components/brewery-search-results.js
+++ src/components/brewery-search-results.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { BrewerySearchResult } from './brewery-search-result';
+import '../styles/components/brewery-search-results.css';
 
 export const BrewerySearchResults = ({ breweries }) => {
   return (
-    <div className="results w-100 mt-4">
+    <div className="brewery-search-results">
       {breweries.length > 0 && (
         <ul className="list-group">
           {breweries.map((brewery) => (
